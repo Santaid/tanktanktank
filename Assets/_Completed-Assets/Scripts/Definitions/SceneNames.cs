3c2fd293a6a97715c3d3da6cdfb506c359441995
ktpoint@outlook.jp <ktpoint@outlook.jp>
2024-10-17 01:46:54 +0900

    名前を多少変更

--- Assets/_Completed-Assets/Scripts/Definitions/SceneNames.cs
+++ Assets/_Completed-Assets/Scripts/Definitions/SceneNames.cs
@@ -6,5 +6,5 @@ public static class SceneNames
 {
     static readonly string TitleScene = "TitleScene";
     static readonly string HomeScene = "HomeScene";
-    static readonly string _Complete-Game = "_Complete-Game";
+    static readonly string CompleteGame = "_Complete-Game";//名前を_Complete-Gameにしたらエラー　ハイフンが原因だと思われる
 }
