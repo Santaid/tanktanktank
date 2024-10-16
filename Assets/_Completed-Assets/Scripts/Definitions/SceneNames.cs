89c8079dab8acf5bb533eb3ec77901bda618a1a9
ktpoint@outlook.jp <ktpoint@outlook.jp>
2024-10-17 01:37:31 +0900

    基本的な画面遷移の3.まで実行

---
+++ Assets/_Completed-Assets/Scripts/Definitions/SceneNames.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneNames
+{
+    static readonly string TitleScene = "TitleScene";
+    static readonly string HomeScene = "HomeScene";
+    static readonly string _Complete-Game = "_Complete-Game";
+}
