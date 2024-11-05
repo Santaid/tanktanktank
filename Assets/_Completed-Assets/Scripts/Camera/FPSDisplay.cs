2c09289a18d41e5fd8f8e57e0543d8942bba5b60
ktpoint@outlook.jp <ktpoint@outlook.jp>
2024-11-05 12:07:23 +0900

    FPSの最小値を表示、Gキーで表示非表示を変更

--- Assets/_Completed-Assets/Scripts/Camera/FPSDisplay.cs
+++ Assets/_Completed-Assets/Scripts/Camera/FPSDisplay.cs
@@ -9,17 +9,31 @@ public class FPSDisplay : MonoBehaviour
     private float elapsedTime;
     private float fps;
 
+    private float minfps = float.MaxValue;
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            if (fpsText.enabled == false)
+            {
+                fpsText.enabled = true;    
+            }
+            else
+            {
+                 fpsText.enabled = false;   
+            }
+        }
+
         frameCount++;
         elapsedTime += Time.deltaTime;
 
         if (elapsedTime > 1.0f)
         {
             fps = frameCount / elapsedTime;
+            if (fps < minfps){minfps = fps;}
             frameCount = 0;
             elapsedTime = 0;
-            fpsText.text = $"FPS: {fps:F2}";
+            fpsText.text = $"FPS: {fps:F2}\nMINFPS:{minfps:F2}";
         }
     }
 }
